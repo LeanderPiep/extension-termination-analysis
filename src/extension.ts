d083d7f47ed5221c0479914140c084fb0c70631a
LeanderPiep <leander.piepenbring@gmail.com>
2026-01-02 19:43:34 +0100

    added a new command to start the extension

--- src/extension.ts
+++ src/extension.ts
@@ -6,20 +6,35 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "termination-analysis" is now active!');
+	const disposable = vscode.commands.registerCommand(
+    'termination-analysis.run',
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('termination-analysis.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from termination-analysis!');
-	});
+      const document = editor.document;
+      const selection = editor.selection;
+      const position = selection.active;
 
-	context.subscriptions.push(disposable);
+      const wordRange = document.getWordRangeAtPosition(position);
+      if (!wordRange) {
+        vscode.window.showWarningMessage(
+          'No function name selected.'
+        );
+        return;
+      }
+
+      const functionName = document.getText(wordRange);
+
+      vscode.window.showInformationMessage(
+        `Termination Analysis triggered for function: ${functionName}`
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
